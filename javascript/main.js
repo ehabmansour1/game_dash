c8195d7677533e006fbdd255d81022df4e130b46
Ehab Mansour <81818344+ehabmansour1@users.noreply.github.com>
2022-03-06 18:41:45 +0200

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,24 @@
+// -----------------------------------start menu
+bars.onclick = function () {
+  nav.classList.add("nav-opened");
+};
+
+bars.addEventListener("click", method1);
+bars.addEventListener("click", method2);
+function method2() {
+  nav.classList.add("nav-opened");
+}
+function method1() {
+  exit.classList.add("animat-exit");
+}
+
+exit.addEventListener("click", method4);
+exit.addEventListener("click", method5);
+function method5() {
+  nav.classList.remove("nav-opened");
+}
+function method4() {
+  exit.classList.remove("animat-exit");
+}
+
+// -----------------------------------end menu
